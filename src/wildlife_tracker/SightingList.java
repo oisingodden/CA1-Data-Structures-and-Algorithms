5b040df6787c52382f8065a5a15059e86239f540
oisingodden <x24271187@student.ncirl.ie>
2026-03-16 13:27:15 +0000

    implemented get, add, remove, get all methods for single link list

--- src/wildlife_tracker/SightingList.java
+++ src/wildlife_tracker/SightingList.java
@@ -18,5 +18,76 @@ public class SightingList {
         size = 0;
     }
     
+    public WildLifeSighting get(int index){ 
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        
+        Node current = head;
+        for (int i = 0; i < index; i++){
+            current = current.getNext();
+        }
+        return current.getData();
+    }
+    
+    public WildLifeSighting remove(int index) {
+        if (index < 0 || index >= size) {
+            return null;
+        }
+        WildLifeSighting removeData;
+        
+        if (index == 0) {
+            removeData = head.getData();
+            head = head.getNext();
+        }else{
+            Node current = head;
+            for (int i = 0; i < index - 1; i++){
+                current = current.getNext();
+            }
+            removeData = current.getNext().getData();
+            current.setNext(current.getNext().getNext());
+        }
+        
+        size --;
+        
+        return removeData;
+    }
+    
+    public void add(WildLifeSighting sighting) {
+        Node newNode = new Node(sighting);
+        
+        if (head == null){
+            head = newNode;
+        }else{
+            Node current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+        
+        size++;
+    }
+    
+    public String getAllSightings() {
+        String result = "";
+        Node current = head;
+        int index = 0;
+        
+        while (current != null){
+            result = result + index + current.getData().getSummary();
+            current = current.getNext();
+            index++;
+        }
+        return result;
+    }
+    
+    public int size(){
+        return size;
+    }
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
     
 }
