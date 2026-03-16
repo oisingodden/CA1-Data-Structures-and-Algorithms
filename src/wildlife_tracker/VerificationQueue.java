ae55e96f7f179e9e8b6e808ea960170b8a6f3308
oisingodden <x24271187@student.ncirl.ie>
2026-03-16 17:44:58 +0000

    VerificationQueue file

--- src/wildlife_tracker/VerificationQueue.java
+++ src/wildlife_tracker/VerificationQueue.java
@@ -4,10 +4,34 @@
  */
 package wildlife_tracker;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author oisinclarke
  */
 public class VerificationQueue {
     
+    private Queue<Integer> queue;
+    
+    public VerificationQueue() {
+        queue = new LinkedList<>();
+    }
+    
+    public void offer(Integer index){
+        queue.offer(index);
+    }
+    
+    public Integer peek(){
+        return queue.peek();
+    }
+    
+    public boolean isEmpty(){
+        return queue.isEmpty();
+    }
+    
+    public int size(){
+        return queue.size();
+    }
 }
